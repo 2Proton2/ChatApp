515b3e0eb8c65a11c1e6acfcbffc4a89b9e61b35
2Proton2 <apurva22kalvade@gmail.com>
2024-02-03 00:51:24 +0530

    feat: add chat, user, user models

---
+++ webapp-server/src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: "String",
+            required: true
+        },
+        email: {
+            type: "String",
+            unique: true,
+            required: true
+        },
+        password: {
+            type: "String",
+            required: true
+        },
+        pic: {
+            type: "String",
+            required: true,
+            default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timestaps: true
+    }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        { id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+};
+
+export const User = mongoose.model("User", userSchema);
