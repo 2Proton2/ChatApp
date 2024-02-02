33fc7862fe53900efd136315808079770e26f910
2Proton2 <apurva22kalvade@gmail.com>
2024-02-03 04:02:51 +0530

    feat: login api

--- webapp-server/src/controllers/user.controller.js
+++ webapp-server/src/controllers/user.controller.js
@@ -1,7 +1,13 @@
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiResponse } from "../utils/ApiResponse.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const userExistsFn = async (emailId) => {
+    return await User.findOne({
+        email: emailId
+    });
+}
 
 export const userRegistration = asyncHandler(async (req, res) => {
     const { name, email, password, pic } = req.body;
@@ -10,9 +16,7 @@ export const userRegistration = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Please Enter all the Feilds", []);
     }
 
-    const userExists = await User.findOne({
-        email
-    });
+    const userExists = await userExistsFn(email);
 
     if (userExists) {
         throw new ApiError(409, "User with email or username already exists", []);
@@ -47,12 +51,36 @@ export const userRegistration = asyncHandler(async (req, res) => {
 
 export const fetchSearchedUser = asyncHandler(async (req, res) => {
     const keyword = req.query.search ? {
-        $or : [
-          {name: {$regex: req.query.search, $options: 'i'}},
-          {email: {$regex: req.query.search, $options: 'i'}}
+        $or: [
+            { name: { $regex: req.query.search, $options: 'i' } },
+            { email: { $regex: req.query.search, $options: 'i' } }
         ]
-      } : {};
+    } : {};
 
-      const users = await User.find(keyword).find({_id: {$ne : req.user.id}});
-      res.send(new ApiResponse(200, users, "Details Fetched Successfully"))
+    const users = await User.find(keyword).find({ _id: { $ne: req.user.id } });
+    res.send(new ApiResponse(200, users, "Details Fetched Successfully"))
 });
+
+export const userLogin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new ApiError(400, "Please Enter all the Feilds", []);
+    }
+
+    const userExists = await userExistsFn(email);
+
+    if ((userExists) && (await userExists.matchPassword(password))) {
+        res.status(200).json(new ApiResponse(200, {
+            _id: userExists._id,
+            name: userExists.name,
+            email: userExists.email,
+            isAdmin: userExists.isAdmin,
+            pic: userExists.pic,
+            token: await userExists.generateAccessToken(userExists._id)
+        }, "Logged in Successfully"));
+    }
+    else {
+        throw new ApiError(401, "Invalid Credentials")
+    }
+})
--- webapp-server/src/routes/user.route.js
+++ webapp-server/src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { userRegistration, fetchSearchedUser } from "../controllers/user.controller.js";
+import { userRegistration, fetchSearchedUser, userLogin } from "../controllers/user.controller.js";
 import { authorization } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
+router.post('/login', userLogin);
 router.route('/').post(userRegistration).get(authorization, fetchSearchedUser);
 
 export default router;
